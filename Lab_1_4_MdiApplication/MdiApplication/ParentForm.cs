425cf331f9cfaf0f67ee32fe72867382fc467d91
Allkeito <rospikachu@gmail.com>
2026-02-15 19:37:41 +0300

    ЛабораторнаяРабота1_Упражнение4

---
+++ Lab_1_4_MdiApplication/MdiApplication/ParentForm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MdiApplication
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments = 0;
+
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+
+            newChild.Show();
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+    }
+}
